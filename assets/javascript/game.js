e65029700e86ba8d0fc542e5bd9487736bf09f8f
Judy Hu <judy.hu9@gmail.com>
2018-10-13 12:44:11 -0400

    beginning

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  //Global Variables
+  var numbers = [];
+
+  var randomNumber;
+  var randomRed;
+  var randomBlue;
+  var randomYellow;
+  var randomGreen;
+
+  var wins = 0;
+  var losses = 0;
+  var totalScore = 0;
+
+  var crystalImages = [
+    "./assets/images/red.jpg",
+    "./assets/images/blue.jpg",
+    "./assets/images/yellow.jpg",
+    "./assets/images/green.jpg"
+  ];
+  //Dom manipulation - JQuery
+});
