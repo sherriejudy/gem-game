c365ba63140ba7fa08ca674e11d24499fae198bf
Judy Hu <judy.hu9@gmail.com>
2018-10-16 17:41:07 -0400

    images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //
-//computer chooses random number between 19-120 and displays on screen
+//computer chooses random number between 19-120 and displays on screen//
 //each crystal has hidden value between 1-9
 //when player clicks on crystal, the value is added to total score
 //player wins when their points add up to total score
@@ -10,29 +10,34 @@
 $(document).ready(function() {
   //Global Variables
   var randomnum = Math.floor(Math.random() * (120 - 19) + 19);
-  var red = Math.floor(Math.random() * (9 - 1) + 1);
-  var blue = Math.floor(Math.random() * (9 - 1) + 1);
-  var yellow = Math.floor(Math.random() * (9 - 1) + 1);
-  var green = Math.floor(Math.random() * (9 - 1) + 1);
 
   var wins = 0;
   var losses = 0;
   var totalScore = 0;
 
   var crystalImages = [
-    "./assets/images/red.jpg",
-    "./assets/images/blue.jpg",
-    "./assets/images/yellow.jpg",
-    "./assets/images/green.jpg"
+    "../images/red.jpg",
+    "../images/blue.jpg",
+    "../images/yellow.jpg",
+    "../images/green.jpg"
   ];
 
   //Dom manipulation - JQuery
   $("#random").text(randomnum);
-  $("#red").text(red);
-  $("#blue").text(blue);
-  $("#yellow").text(yellow);
-  $("#green").text(green);
+  $("#crystals").text(crystalImages);
   $("#totalscore").text(totalScore);
   $("#wins").text(wins);
   $("#losses").text(losses);
+
+  //Main////////////////////////////////////////////////////////////////////////////////
+  function setData() {
+    var image = $("<img>");
+    image.addClass("ghostImage");
+    image.attr("src", crystalImages[i]);
+    image.attr("data-crystalvalue"), Math.floor(Math.random() * (12 - 1) + 1);
+    $("#crystals").append(image);
+  }
 });
+
+// for loop that interates through random crystal value 4 times
+// function that wil seperate img element for me <img>??
