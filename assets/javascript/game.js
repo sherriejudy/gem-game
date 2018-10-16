5a266c4fa33964e7376f01b6acf12b14eadc6d6a
Judy Hu <judy.hu9@gmail.com>
2018-10-16 16:28:51 -0400

    global variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,16 @@
+//
+//computer chooses random number between 19-120 and displays on screen
+//each crystal has hidden value between 1-9
+//when player clicks on crystal, the value is added to total score
+//player wins when their points add up to total score
+//player loses when their points goes above random number
+//resets game when wins or losses occur
+//new game should display new random number and new hidden values for crystals
+
 $(document).ready(function() {
   //Global Variables
-  var numbers = [];
-
-  var randomNumber;
-  var randomRed;
-  var randomBlue;
-  var randomYellow;
-  var randomGreen;
+  var randomnum = Math.floor(Math.random() * (120 - 19) + 19);
+  var crystalnum = Math.floor(Math.random() * (9 - 1) + 1);
 
   var wins = 0;
   var losses = 0;
@@ -18,5 +22,14 @@ $(document).ready(function() {
     "./assets/images/yellow.jpg",
     "./assets/images/green.jpg"
   ];
+
   //Dom manipulation - JQuery
+  $("#random").text(randomnum);
+  $("#red").text(crystalnum);
+  $("#blue").text(crystalnum);
+  $("#yellow").text(crystalnum);
+  $("#green").text(crystalnum);
+  $("#totalscore").text(totalScore);
+  $("#wins").text(wins);
+  $("#losses").text(losses);
 });
